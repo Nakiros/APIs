2b352d859ba7e198052ed1cb478b0ce52a68a1e4
Nakiros <costi.nicolescu@gmail.com>
2022-11-22 14:30:14 +0200

    not_finished

--- first_fastApi_2.py
+++ first_fastApi_2.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from typing import Optional
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Itemi(BaseModel):
+    Denumire: str
+    Pret: float
+    Producator: Optional[str] = None
 
 @app.get("/")
 def home():
@@ -24,12 +31,19 @@ inventory = {
 }
 
 @app.get("/get-item/{produs_id}")
-def get_item(produs_id: int=Path(None, description="The Id of the product you want to look-up.",gt=0, lt=3)):
+def get_item(produs_id: int=Path(None, description="The Id of the product you want to look-up.",gt=0)):
     return inventory[produs_id]
 
 @app.get("/get-by-name")
-def get_item(Denumire_n: Optional[str] = None):                       #parameter is optional
+def get_item(Denumire: str = Query(None, title="Denumire", description="name of product",max_length=10, min_length=2)):     
     for produs_id in inventory:
-        if inventory[produs_id]["Denumire"] == Denumire_n:
+        if inventory[produs_id].Denumire == Denumire:
             return inventory[produs_id]
     return {"Data":"Not found"}
+
+@app.post("/create-item/{produs_id}")
+def create_item(produs_id:int, item: Itemi):
+    if produs_id in inventory:
+        return {"Error":"Produs_id already exists"}
+    inventory[produs_id]=item
+    return inventory[produs_id]
